2cd9a0e0997aca39927d9740d5513f6f2fc09ac9
Kevinoo5 <kevin.noormets@voco.ee>
2023-01-31 01:20:39 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const path = require("path")
 const fetch = require("node-fetch")
 const {response} = require("express");
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
+
 
 const key = "b9b27e0c6774d5bc94269cbec554bdbe"
 let city = "Tartu"
@@ -23,8 +27,28 @@ app.get("/", (req, res) => {
     res.render("index", {
         description: description,
         city: city,
-        temp: temp,
+        temp: temp
     })
+        })
+})
+
+app.post("/", function (req,res){
+    let city = req.body.cityname
+    console.log(req.body.cityname)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+
+            res.render("index", {
+                description: description,
+                city: city,
+                temp: temp
+            })
         })
 })
 app.listen(3000)
